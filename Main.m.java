bfe4c9e356b3364e3f40d5074eab12ee7db62926
markallanfederico <markietalaymago24@gmail.com>
2024-10-04 15:30:25 +0800

    Add files via upload

---
+++ Main.m.java
@@ -0,0 +1,11 @@
+public class Main
+{
+
+	public static void main(String[] args) {
+		bicycle sportBicycle = new bicycle();
+		
+		
+		System.out.println(sportBicycle.gear);
+		sportBicycle.braking();
+		}
+}
---
+++ bicycle.m.java
@@ -0,0 +1,11 @@
+public class bicycle 
+{
+	public int gear;
+	
+	public bicycle(){
+	gear = 5;
+	}
+	public void braking() {
+		System.out.print("Braking");
+	}
+}	
